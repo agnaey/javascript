0fbbe71ac642f26a24d2f328dc16b96762aad29b
agnaey <agnaeyviswanath1234@gmail.com>
2024-12-09 13:33:19 +0530

    js basic learned

---
+++ basic/basic1.js
@@ -0,0 +1,74 @@
+// alert('oo my god')
+
+
+// console.log('this will show on console in inspect')
+
+// a='hi'
+// alert(a)
+
+
+
+
+
+
+// --------------datatypes---------
+
+
+
+// var a='hello'     //string
+// var a=123         //number
+
+// const a=[1,2,3,4]       // object
+// const a={name:'agnaey',age:21}
+
+// var a=[]           //undefine
+
+
+
+
+// console.log(typeof a);
+// console.log(a);
+
+// ----------------------------operators----------------------------
+
+//   arthamatic
+
+// let a=5       
+// ++a
+// --a
+// console.log(a)
+
+
+//       assigment
+
+// let a=10
+
+// a**=10
+// a*=10
+// a+=10
+// a-=10
+// a/=10
+// a%=10
+// console.log(a)
+
+// ------comparison------
+
+// let a=10
+// let b=20
+
+// console.log (a===b);
+// console.log(a==b)
+// console.log(a!==b)
+// console.log(a!=b)
+
+// -----------------logical-----------------
+// (&&- AND)  (|| - or)
+
+
+let a=10
+let b=20
+
+console.log(a!=b || a==0)
+console.log(a!=b && a==10)
+
+
